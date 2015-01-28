ab1337561bf7fbf860f2cea1be76fe1c18e51651
Pete Corey <petecorey@gmail.com>
2015-01-28 15:12:15 -0800

    Rough layout

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/main.css': 'sass/main.scss'
+                }
+            }
+        },
+        autoprefixer: {
+            dist: {
+                src: 'css/main.css',
+                dest: 'css/main.css'
+            }
+        },
+        cssmin: {
+            compress: {
+                files: {
+                    'css/main.css': ['css/main.css']
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['sass/**/*.scss'],
+                tasks: ['sass', 'autoprefixer', 'cssmin']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+};
