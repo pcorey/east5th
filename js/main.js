b975544762bc048cce72feb20be9c628fffda7c0
Pete Corey <petecorey@gmail.com>
2015-01-29 15:11:09 -0800

    New layout

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-function buildControlClickHandler(tab) {
-    return function(e) {
-        document.getElementsByClassName('active control')[0].className = 'control';
-        e.target.className = 'active control';
-    };
-}
+$(function() {
 
-var controls = document.getElementsByClassName('control');
-for (var i = 0; i < controls.length; i++) {
-    var control = controls[i];
-    var tab = control.getAttribute('data-tab');
-    control.onclick = buildControlClickHandler(tab);
-}
+    function buildChangeHandler(element) {
+        return function change(e) {
+            if (element.val()) {
+                $(element.attr('data-next')).addClass('visible');
+            }
+        };
+    }
+
+    $('[data-next]').each(function() {
+        $(this).change(buildChangeHandler($(this)));
+    })
+});
